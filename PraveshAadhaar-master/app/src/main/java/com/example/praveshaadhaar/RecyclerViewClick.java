4b51f40dc6c8d646c40e8e6e3ab062c49bd0dc0c
soumyabhardwaj <63638319+soumyabhardwaj@users.noreply.github.com>
2021-05-29 15:39:51 +0530

    Delete PraveshAadhaar-master directory

--- PraveshAadhaar-master/app/src/main/java/com/example/praveshaadhaar/RecyclerViewClick.java
+++
@@ -1,5 +0,0 @@
-package com.example.praveshaadhaar;
-
-public interface RecyclerViewClick {
-    void onItemClick(int position, float san,float mask,float dist,String shopName,String shopAddress);
-}
